a21ab678b26072c62853eb3db641e57a78afc70b
Damon Kaswell <gooddamon@gmail.com>
2018-03-18 11:22:59 -0700

    Initial application creation

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/default.js');
+const routes = require('./config/routes.js');
+const logger = require('./lib/logger.js');
+const auth = require('./lib/auth.js');
+const dbInterface = require('./lib/dbInterface.js');
+
+app.set('config', config);
+app.set('routes', routes);
+app.set('auth', auth);
+app.set('dbInterface', dbInterface);
+
+app.use(logger);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('static'));
+
+require('./lib/api.js')(app);
+
+app.listen(3000, () => {
+    console.log('StoryTracker API started on port 3000.');
+});
---
+++ config/routes.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'API_LOGIN': '/api/login',
+    'API_STORIES': '/api/stories',
+    'API_STORY': '/api/stories/:storyId',
+    'API_SUBMISSIONS': '/api/stories/:storyId/submissions',
+    'API_SUBMISSION': '/api/stories/:storyId/submissions/:subId',
+    'API_READERS': '/api/stories/:storyId/readers',
+    'API_READER': '/api/stories/:storyId/readers/:readerId'
+};
